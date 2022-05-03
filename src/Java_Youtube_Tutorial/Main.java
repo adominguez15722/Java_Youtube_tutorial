eaec01a840b557629620aad72092537c5fa9b07f
Anthony Dominguez <adominguez15722@gmail.com>
2022-05-03 11:39:10 -0500

    initial commit

---
+++ src/Java_Youtube_Tutorial/Main.java
@@ -0,0 +1,14 @@
+package Java_Youtube_Tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] newArr = new String[5];
+//        {"hello", "hi", "Tim", "bill", "joe"}
+
+        newArr[0] = "hello";
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
